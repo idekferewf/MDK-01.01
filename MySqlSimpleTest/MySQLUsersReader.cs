c136bbd156de844082fa6bc5b2e7fec9584c9b5d
st56 <example@gmail.com>
2025-04-03 13:00:55 +0300

    добавлен метод для добавления пользователя в базу данных

--- MySqlSimpleTest/MySQLUsersReader.cs
+++ MySqlSimpleTest/MySQLUsersReader.cs
@@ -8,6 +8,30 @@ namespace MySqlSimpleTest
     {
         private string myConnectionString = "server=127.0.0.1;uid=root;pwd=vertrigo;database=my_vk_network;";
 
+        public bool AddUser(User user)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(myConnectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO users (login, password, fio, age, birth_date, email) " +
+                        "VALUES (" +
+                        $"'{user.Login}', '{user.Password}', '{user.Fio}', '{user.Age}', '{user.BitrhDate.ToString("yyyy-MM-dd")}', '{user.Email}'" +
+                        ");";
+                    MySqlCommand command = new MySqlCommand(query, conn);
+                    MySqlDataReader reader = command.ExecuteReader();
+                    reader.Close();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+                return false;
+            }
+            return true;
+        }
+
         public bool DeleteUser(string login)
         {
             try
